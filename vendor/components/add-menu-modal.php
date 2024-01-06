0b7e27416b7b8158c33feee9d1cffa2056897268
IqramRafie <iqram.mohdrafie@gmail.com>
2024-01-06 20:03:31 +0800

    Add Menu Fucntion

--- vendor/components/add-menu-modal.php
+++ vendor/components/add-menu-modal.php
@@ -23,12 +23,16 @@
                         <input type="text" name="menuname" id="menuname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Food Name" required="">
                     </div>
                     <div class="col-span-2">
-                        <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                        <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="5.50" required="">
+                        <label for="menuname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Menu Name</label>
+                        <textarea name="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Description" required=""></textarea>
                     </div>
                     <div class="col-span-2">
-                        <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="number" name="quantity" id="quantity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="50 etc" required="">
+                        <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                        <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="5.50" required step=".01" min="0" value="0">
+                    </div>
+                    <div class="col-span-2">
+                        <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity</label>
+                        <input type="number" name="quantity" id="quantity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="50 etc" required>
                     </div>
                 </div>
                 <button id="add-menu-submit" type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -46,13 +50,15 @@ $(document).ready(function() {
     $('#add-menu-form').submit(function(e) {
         e.preventDefault();  
         var name = $('#menuname').val();
+        var description = $('#description').val();
         var price = $('#price').val();
-        var quantity = $('#quanitty').val();
+        var quantity = $('#quantity').val();
         $.ajax({
             type: "POST",
             url: "../php_function/addmenu.php",
             data: {
                 name: name,
+                description: description,
                 price: price,
                 quantity: quantity
             },
