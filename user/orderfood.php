8f0a7453305d52c5a9f4f4b32376aac11841c091
aizuddin09 <aizuddinrosli09@gmail.com>
2024-01-08 14:30:20 +0800

    judin

--- user/orderfood.php
+++ user/orderfood.php
@@ -1,6 +1,46 @@
 <?php
 include '../php_function/authPage.php';
 include '../php_function/order.php';
+
+?>
+<?php
+
+if(isset($_POST['add_to_cart'])){
+$item_name = $_POST['item_name'];
+$item_price = $_POST['item_price'];
+$item_description = $_POST['item_description'];
+$item_kiosk = $_POST['item_kiosk'];
+$item_image = $_POST['item_image'];
+$item_quantity = $_POST['item_quantity'];
+
+$select_cart = mysqli_query($conn, "SELECT * FROM `Order` WHERE name = '$item_name'") or die('query failed');
+
+if(mysqli_num_rows($select_cart) > 0){
+   $message[] = 'product already added to cart!';
+}else{
+  mysqli_query($conn, "INSERT INTO `Order`(user_id, kiosk_id, name, price, description, quantity) VALUES('$user_id', '$kiosk_id', '$item_name', '$item_price', '$item_description', '$item_quantity')") or die('query failed');
+    $message[] = 'product added to cart!';
+}
+
+};
+
+if(isset($_POST['update_cart'])){
+$update_quantity = $_POST['cart_quantity'];
+$update_id = $_POST['cart_id'];
+mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_quantity' WHERE id = '$update_id'") or die('query failed');
+$message[] = 'cart quantity updated successfully!';
+}
+
+if(isset($_GET['remove'])){
+$remove_id = $_GET['remove'];
+mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'") or die('query failed');
+header('location:index.php');
+}
+
+if(isset($_GET['delete_all'])){
+mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+header('location:index.php');
+}
 ?>
 
 
@@ -38,6 +78,43 @@ include '../php_function/order.php';
 
 
 <!-- Main Body -->
+<!-- Item -->
+<div class="menu">
+
+   <h1 class="heading">Choose Food You Want</h1>
+
+   <div class="box-container">
+
+   <?php
+      $select_menu = mysqli_query($conn, "SELECT * FROM `Menu`") or die('query failed');
+      if(mysqli_num_rows($select_menu) > 0){
+         while($fetch_menu = mysqli_fetch_assoc($select_menu)){
+   ?>
+      <form method="post" class="box" action="">
+         <img src="images/<?php echo $fetch_menu['image']; ?>" alt="">
+         <div class="name"><?php echo $fetch_menu['name']; ?></div>
+         <div class="description"><?php echo $fetch_menu['description']; ?></div>
+         <div class="kiosk"><?php echo $fetch_menu['kiosk_id']; ?></div>
+         <div class="price">RM<?php echo $fetch_menu['price']; ?>/-</div>
+         <input type="number" min="1" name="item_quantity" value="0">
+         <input type="hidden" name="item_image" value="<?php echo $fetch_menu['image']; ?>">
+         <input type="hidden" name="item_name" value="<?php echo $fetch_menu['name']; ?>">
+         <input type="hidden" name="item_price" value="<?php echo $fetch_menu['price']; ?>">
+         <input type="hidden" name="item_description" value="<?php echo $fetch_menu['description']; ?>">
+         <input type="hidden" name="item_kiosk" value="<?php echo $fetch_menu['kiosk_id']; ?>">
+         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+      </form>
+   <?php
+      };
+   };
+   ?>
+
+   </div>
+
+</div>
+
+<!-- Main Body -->
+
 
 
 
